746a245e18abc97e2884aeafc4e50f7e59386ebf
Nahum Francisco Cortés Cervantes <nahum@macbook-pro-de-nahum.local>
2016-04-28 21:50:39 -0500

    Initial Commit

---
+++ hamburguesas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hamburguesas
+//
+//  Created by Nahum Francisco Cortés Cervantes on 28/04/16.
+//  Copyright © 2016 Nahum Francisco Cortés Cervantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
